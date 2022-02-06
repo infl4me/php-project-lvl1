a19a8e6c8ee1a3309f6ed61bee0ad5aacb3f6ba5
udf <hellpl4y@gmail.com>
2022-02-06 20:36:28 +0500

    add brain-calc

--- src/getQuestion.php
+++ src/getQuestion.php
@@ -6,6 +6,34 @@ use function Brain\Games\utils\isEven;
 
 function getBrainEvenQuestionData()
 {
-    $question = rand(0, 1000);
+    $question = rand(0, 100);
     return [$question, isEven($question) ? 'yes' : 'no'];
+}
+
+function getBrainCalcQuestionData()
+{
+    $op1 = rand(0, 10);
+    $op2 = rand(0, 10);
+    $operation = rand(0, 2);
+    $operations = ['+', '-', '*'];
+
+    $answer = null;
+    switch ($operation) {
+        case 0:
+            $answer = $op1 + $op2;
+            break;
+        case 1:
+            $answer = $op1 - $op2;
+            break;
+        case 2:
+            $answer = $op1 * $op2;
+            break;
+
+        default:
+            break;
+    }
+
+    $question = "{$op1} {$operations[$operation]} {$op2}";
+
+    return [$question, (string) $answer];
 }
