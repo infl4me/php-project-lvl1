5bbfbe95a04712234c4f594ffadbce6776db4741
udf <hellpl4y@gmail.com>
2022-02-06 21:30:45 +0500

    fix linter errors

--- src/Engine.php
+++ src/Engine.php
@@ -2,33 +2,33 @@
 
 namespace Brain\Games\Engine;
 
+use function Brain\Games\Cli\runGreeting;
 use function cli\line;
 use function cli\prompt;
-use function Brain\Games\Cli\runGreeting;
 
 function run($questionDscr, $getData)
 {
-  $roundsCount =  3;
-  $name = runGreeting();
+    $roundsCount = 3;
+    $name = runGreeting();
 
-  line($questionDscr);
+    line($questionDscr);
 
-  for ($i = 0; $i < $roundsCount; $i++) {
-    [$question, $answer] = $getData();
-    line("Question: %s", $question);
+    for ($i = 0; $i < $roundsCount; $i++) {
+        [$question, $answer] = $getData();
+        line("Question: %s", $question);
 
-    $userAnswer = prompt('Your answer');
-    if ($userAnswer === $answer) {
-      line("Correct!");
-    } else {
-      line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$answer}'.");
-      line("Let's try again, {$name}!");
+        $userAnswer = prompt('Your answer');
+        if ($userAnswer === $answer) {
+            line("Correct!");
+        } else {
+            line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$answer}'.");
+            line("Let's try again, {$name}!");
 
-      return $i + 1;
+            return $i + 1;
+        }
     }
-  }
 
-  line("Congratulations, {$name}!");
+    line("Congratulations, {$name}!");
 
-  return $i + 1;
+    return $i + 1;
 }
